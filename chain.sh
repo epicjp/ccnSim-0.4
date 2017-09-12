d63963dbd7dbb46cc34f1879683fe3df92213546
epicjp <quocdinh.nguyen@gmail.com>
2017-09-12 02:17:23 +0000

    Add chain.sh

---
+++ chain.sh
@@ -0,0 +1,2 @@
+#./runsim_script_ED_TTL.sh chain_6_1_1 1 1 spr lce lru 1 1e5 1e5 1e8 1e8 20 IRM IRM 1 0 cold naive 0.75 1
+./runsim_script_ED_TTL.sh chain_6_1_1_rate 1 1 spr lce lru 1 1e5 1e5 1e8 1e8 20 IRM IRM 1 0 cold naive 0.75 1
